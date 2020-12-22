01355d5f54dbd80fc640a51885a84a2a25c6c3d8
MattPlayGamez <62027538+MattPlayGamez@users.noreply.github.com>
2020-12-22 11:31:10 +0100

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-BEFT43J5PH"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-BEFT43J5PH');
-</script>
