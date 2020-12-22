35438fb9a612af34fccf374ccadfc242afb01c91
MattPlayGamez <62027538+MattPlayGamez@users.noreply.github.com>
2020-12-22 11:30:02 +0100

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-BEFT43J5PH"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-BEFT43J5PH');
+</script>
